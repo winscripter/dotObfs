f64911032db724fb71612c8eda3e763d0ddb3196
winscripter <142818255+winscripter@users.noreply.github.com>
2023-09-21 12:05:42 +0400

    Modify the homemade AssemblyInfo.cs

--- dotObfs/dotObfs/Properties/AssemblyInfo.cs
+++ dotObfs/dotObfs/Properties/AssemblyInfo.cs
@@ -1,3 +1,13 @@
+// Do not copy this file if you wish to add custom
+// assembly information with your IDE.
+//
+// To change assembly information with Visual Studio:
+//   In solution explorer, right click your project, and
+//   select Properties
+//
+//   Click Assembly Information, and change anything to your
+//   preferences.
+//
 using System.Reflection;
 
 [assembly: AssemblyTitle("dotObfs - An open-source .NET obfuscator library")]
